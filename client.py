67bde0f46e8df13239b9ffc7583affecf489c798
Nyíri Dániel <nyiridaniel3@gmail.com>
2025-04-22 22:58:21 +0200

    feat: add transaction request handle
    details.json contains all the necessary datas.

--- client.py
+++ client.py
@@ -1,17 +1,28 @@
 import socket
+import random
+import json
 
 HOST = '127.0.0.1'
 PORT = 65432
+
+# read .json file
+with open('details.json', 'r') as file:
+  details = json.load(file)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.connect((HOST, PORT))
   print('[C] Connected to the server')
 
-  msg = 'Hello Server'.encode()
-  s.sendall(msg)
-  print('[C] Message sent to the server')
+  phoneNo = random.choice(details['phoneNumbers'])
+  amount = random.choice(details['amounts'])
+  s.send(f'REQ,{phoneNo},{amount}'.encode())
+  print(f'[C] >>> request: {phoneNo} <-- {amount} Ft')
 
-  data = s.recv(1024)
-  print('[C] Acknowledge recieved from the server')
+  ack = s.recv(1024).decode().split(',')
+  if ack[0] == 'OK':
+    tid = ack[1]
+    s.send(f'CONF,{tid},{phoneNo},{amount}'.encode())
+    print(f'[S] >>> status: {s.recv(1024).decode()}')
 
-print(f'[C] Recieved data: {data.decode()}')
+  elif ack[0] == 'ERR':
+    print('[S] >>> status: ERROR')
--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 import socket
+import time
+import uuid
+import json
 
 HOST = '127.0.0.1'
 PORT = 65432
+
+# read .json file
+with open('details.json', 'r') as file:
+  details = json.load(file)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.bind((HOST, PORT))
@@ -14,10 +21,30 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
       print(f'[S] Connected from {addr} address')
 
       while True:
-        data = conn.recv(1024)
+        data = conn.recv(1024).decode()
         if not data:
           break
-        print(f'[S] address: {addr}')
-        print(f'[S] data: {data.decode()}')
-        res = "OK".encode()
-        conn.sendall(res)
+        
+        parts = data.split(',')
+        time.sleep(1)
+        if parts[0] == 'REQ':
+          phoneNo = parts[1]
+          amount = int(parts[2])
+          print(f'[C] >>> phoneNo: {phoneNo}, amount: {amount}')
+
+          if phoneNo not in details['phoneNumbers'] or amount not in details['amounts']:
+            conn.send('ERR'.encode())
+            print('[S] >>> status: ERROR')
+            continue
+
+          tid = str(uuid.uuid4())
+          conn.send(f'OK,{tid}'.encode())
+          print('[S] >>> status: OK')
+
+        elif parts[0] == 'CONF':
+          conn.send('UPLOADED'.encode())
+          print('[S] >>> status: UPLOADED')
+
+        else:
+          conn.send('INVALID'.encode())
+          print('[S] >>> status: INVALID')
